1add04728a957f3030897b4b733358b4ffd1defb
rifkiazizi <rifkiazizi00@gmail.com>
2020-03-21 12:37:53 +0700

    tugas_oop1

---
+++ src/oop1/Mahasiswa.java
@@ -0,0 +1,18 @@
+package oop1;
+
+/**
+ *
+ * @author rifki
+ */
+public class Mahasiswa {
+    public static void main(String[] args) {
+      String nama = "Rifki Azizi";
+      String kelas = "4C";
+      String nim = "18090020";
+        System.out.println("Nama : "+nama);
+        System.out.println("Kelas : "+kelas);
+        System.out.println("Nim : "+nim);
+        
+    }
+}
+    
